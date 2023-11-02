489fd77b2da09584e324ec7bb4dc4e3d04b2a73f
Freddy <fredericdouamba2014@gmail.com>
2023-11-02 12:00:16 -0700

    Initial Commit

---
+++ DataPersistence/DataPersistenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataPersistenceApp.swift
+//  DataPersistence
+//
+//  Created by Student Account on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct DataPersistenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
