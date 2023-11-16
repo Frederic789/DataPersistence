aaacbfb8b73792b0c6cb5a10747aca8e2622a5c6
Freddy <fredericdouamba2014@gmail.com>
2023-11-15 16:59:45 -0800

    chapter6: Data Persistence

--- DataPersistence/ContentView.swift
+++ DataPersistence/ContentView.swift
@@ -6,19 +6,96 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(entity: Movie.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Movie.title, ascending: true)]) private var movies: FetchedResults<Movie>
+    
+    @State private var movieTitle: String = ""
+    @State private var movieYear: String = ""
+    @State private var movieDirector: String = ""
+    @State private var showAlert = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Toggle("Dark Mode", isOn: $isDarkMode)
+                .padding()
+            
+            TextField("Movie Title", text: $movieTitle)
+            TextField("Year of Release", text: $movieYear)
+                .keyboardType(.numberPad)
+            TextField("Director's Name", text: $movieDirector)
+            
+            Button("Add Movie") {
+                if isValidYear(movieYear) {
+                    addMovie()
+                } else {
+                    showAlert = true
+                }
+            }
+        
+            
+            .padding()
+            
+            .alert(isPresented: $showAlert) {
+                            Alert(title: Text("Invalid Year"), message: Text("Please enter a valid year."), dismissButton: .default(Text("OK")))
+                        }
+
+            List {
+                ForEach(movies, id: \.self) { movie in
+                    VStack(alignment: .leading) {
+                        Text(movie.title ?? "")
+                        Text("\(movie.year)")
+                        Text(movie.director ?? "")
+                    }
+                }
+                .onDelete(perform: deleteMovies)
+            }
         }
-        .padding()
+        .preferredColorScheme(isDarkMode ? .dark : .light)  // Set the color scheme
+    }
+    
+    private func isValidYear(_ year: String) -> Bool {
+           return Int16(year) != nil
+       }
+
+    private func addMovie() {
+            let newMovie = Movie(context: viewContext)
+            newMovie.title = movieTitle
+            newMovie.year = Int16(movieYear) ?? 0
+            newMovie.director = movieDirector
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Handle the Core Data error.
+                print(error.localizedDescription)
+            }
+        }
+
+    private func deleteMovies(at offsets: IndexSet) {
+        for index in offsets {
+            let movie = movies[index]
+            viewContext.delete(movie)
+        }
+
+        do {
+            try viewContext.save()
+        } catch {
+            // Handle the Core Data error.
+            print(error.localizedDescription)
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
 }
+
+
+
--- DataPersistence/PersistenceController.swift
+++ DataPersistence/PersistenceController.swift
@@ -5,4 +5,45 @@
 //  Created by Student Account on 11/2/23.
 //
 
-import Foundation
+
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "DataPersistenceModel")
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
+}
+
+extension PersistenceController {
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        // Here you can add some sample data for the preview
+        for _ in 0..<5 {
+            let newItem = Movie(context: viewContext)
+            newItem.title = "Sample Movie"
+            newItem.year = 2023
+            newItem.director = "Sample Director"
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            // Handle the Core Data error.
+            fatalError("Error: \(error.localizedDescription)")
+        }
+        return result
+    }()
+}
